76b50306f47d76217de96b8525990b9d2b8aec7e
lavanyasomepalli <lavanyasomepalli164@gmail.com>
2024-12-11 21:11:07 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "my_instance" {
-  ami                   ="ami-0166fe664262f664c"
+  ami                   ="ami-0453ec754f44f9a4a"
   instance_type          = "t2.micro"
   count                  = 1
   key_name               = "LAVANYA"
