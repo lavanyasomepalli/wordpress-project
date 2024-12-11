ddd8d2234b819b5454ef88fd63d4653a402edc10
lavanyasomepalli <lavanyasomepalli164@gmail.com>
2024-12-11 11:33:25 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -12,11 +12,6 @@ resource "aws_instance" "my_instance" {
     Name = "My public Instance 1"
   }
 }
-
-resource "aws_security_group" "demosg" {
-  name        = "demosg"
-  description = "Security group for WordPress and MySQL"
-
   ingress {
     from_port   = 80
     to_port     = 80
